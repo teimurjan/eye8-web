b4862e7a2bb11587c72871f4596ecf16c7064cc2
Teimur Gasanov <teymurgg321@gmail.com>
2020-06-18 11:34:25 +0600

    Update next-tag

--- next-tag.sh
+++ next-tag.sh
@@ -2,7 +2,12 @@
 version=$(cat package.json | grep \\\"version\\\" | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 
 if [ "$version" != "" ]; then
-    git tag -a "v$version" -m "`git log -1 --format=%s`"
-    git push --tags
-    echo "Tag v$version pushed"
+    if [ $(git tag -l "v$version") ]; then
+        echo "Tag already exists (v$version)"
+    else
+        git tag -a "v$version" -m "`git log -1 --format=%s`"
+        git push --tags
+        echo "New tag pushed (v$version)"
+    fi
+
 fi
