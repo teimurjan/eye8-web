52fbabf5feee41d0bad4d73b2adb1e9fc5c806c1
Teimur Gasanov <teymurgg321@gmail.com>
2020-07-13 11:53:24 +0600

    Fix node env variable in nex config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ module.exports = withBundleAnalyzer(
       SENTRY_ORG: process.env.SENTRY_ORG,
       SENTRY_PROJECT: process.env.SENTRY_PROJECT,
       SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
-      NODE_ENV: process.env.NODE_ENV,
       RELEASE_VERSION: process.env.RELEASE_VERSION,
       PUBLIC_URL: process.env.PUBLIC_URL,
       CLIENT_API_URL: process.env.CLIENT_API_URL,
