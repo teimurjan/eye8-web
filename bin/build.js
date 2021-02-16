b72c2e80fade6dee4dd4f06d814bde9e9d9c723d
Teimur Gasanov <teymurgg321@gmail.com>
2021-02-16 15:37:12 +0600

    update build config

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const { runSync } = require('./utils');
 
-runSync('lerna run', ['build', '--stream', '--parallel'], { shell: true });
+runSync('lerna run', ['--scope "@eye8/app"', 'build', '--stream', '--parallel'], { shell: true });
